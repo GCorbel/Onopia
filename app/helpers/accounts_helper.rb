0a2a3d1874aceaad71fa1980812fb8dde5b60301
Guirec Corbel <guirec.corbel@gmail.com>
2012-02-03 18:25:56 -0500

    remove all external f files

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,12 +1,2 @@
 module AccountsHelper
-  def category_for_record(category, record, options)
-    select_tag("category_id", 
-      options_from_collection_for_select(
-        options, 'id', 'label',
-        :selected => category.id,
-      ),
-      :class => "record_change_category",
-      :id => "category_id_#{record.id}"
-    )
-  end
 end
