4cb097064ca45f7a4dcab5fa1cf7fcca0a5ac661
Guirec Corbel <guirec.corbel@gmail.com>
2012-02-19 19:51:20 -0500

    add spec for home controller

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,49 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe HomeController do
+  fixtures :all
+  render_views
+  
+  describe "#home" do
+    
+    context "when not logged" do
+      it "should redirect to login" do
+        get :index
+        response.should redirect_to(login_url)
+      end
+    end
+    
+    context "when logged" do
+      setup :activate_authlogic
+      before(:each) do
+        login_user
+      end
+      
+      it "should render the home template" do
+        get :index
+        response.should render_template(:index)
+      end
+    end
+  end
+  
+  describe '#login' do
+    context "when not logged" do
+      it "should render the login template" do
+        get :login
+        response.should render_template(:login)
+      end
+    end
+    
+    context "when logged" do
+      setup :activate_authlogic
+      before(:each) do
+        login_user
+      end
+      
+      it "should redirect to home" do
+        get :login
+        response.should redirect_to(root_url)
+      end
+    end
+  end
+end
